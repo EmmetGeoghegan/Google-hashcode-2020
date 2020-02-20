f8fd49bb107c022b2584b1a50ddc76ae4a4c205b
Daniel Greally <47182737+greally2014@users.noreply.github.com>
2020-02-20 19:45:15 +0000

    Create score.py

---
+++ HashCode 2020/score.py
@@ -0,0 +1,7 @@
+def scoring_function():
+        score = 0
+        already_scanned = []
+        for book_self in all_books):
+            if book_self.shipped == True:
+                score += book_self.scores
+        return score
